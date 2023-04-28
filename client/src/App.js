0c681e8b8052415dfb7721b85d64bd1f7a48fa08
Abhatt18-ui <apurvabhatt261097@gmail.com>
2023-04-28 15:23:40 +0530

    checkout screen and checkout flow

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import LandingScreen from './screens/LandingScreen';
 import LoadingScreen from './screens/LoginScreen';
 import RegistrationScreen from './screens/RegistrationScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import CheckoutScreen from './screens/CheckoutScreen';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
   <Route path='/login' element={<LoadingScreen/>}/>
   <Route path='/registration' element={<RegistrationScreen/>}/>
   <Route path='/profile' element={<ProfileScreen/>}/>
+  <Route path='/checkout' element={<CheckoutScreen/>}/>
 </Routes>        
       </main>
       <Footer/>
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import express from 'express';
 // Our Routes
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 connectToDatabase();
 const app = express();
@@ -13,9 +14,9 @@ const app = express();
 app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 const port = process.env.PORT || 5000;
-const db = process.env.URI;
 
 app.listen(port, ()=> {
     console.log(`server runs on port ${port}.`);
