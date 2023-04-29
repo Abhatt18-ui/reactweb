40dbb7dac8fa60adad63e567501947af4b740ce5
Abhatt18-ui <apurvabhatt261097@gmail.com>
2023-04-29 16:33:03 +0530

    reviews & user orders

--- server/models/product.js
+++ server/models/product.js
@@ -1,62 +1,66 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const reviewSchema = new mongoose.Schema({
-    name:{type: String, require: true},
-    rating:{type: Number, require: true},
-    comment:{type: String, require: true},
-    title:{type: String, require: true},   
-    uesr: {type: mongoose.Schema.Types.ObjectId, requird: true, ref: 'User'},
-}, {timestamps: true});
-
-const productSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-    },
-    image:{
-        type: String,
-        required: true,
-    },
-    brand:{
-        type: String,
-        required: true,
-    },
-    category:{
-        type: String,
-        required: true,
-    },
-    description:{
-        type: String,
-        required: true,
-    },
-    reviews: [reviewSchema],
-    rating:{
-    type: Number,
-    required: true,
-    default: 0,
-    },
-    numberOfReviews:{
-        type: Number,
-        required: true,
-    },
-    brand:{
-        type: Number,
-        required: true,
-        default:0,
-    },
-    stock:{
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    productIsNew:{
-        type: Boolean,
-        default: false,
-    }, 
- },
-{timestamps : true}
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    title: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  },
+  { timestamps: true }
 );
 
-const Product = mongoose.model('Product',productSchema);
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numberOfReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    productIsNew: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
 
 export default Product;
