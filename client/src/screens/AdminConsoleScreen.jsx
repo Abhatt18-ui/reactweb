a88b398792978a0eaec00f2bae5e63b551411699
Abhatt18-ui <apurvabhatt261097@gmail.com>
2023-05-01 12:43:17 +0530

    admin console orders

--- client/src/screens/AdminConsoleScreen.jsx
+++ client/src/screens/AdminConsoleScreen.jsx
@@ -11,33 +11,40 @@ import {
 import { Navigate,useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import UsersTab from '../components/UsersTab';
+import OrdersTab from '../components/OrdersTab';
 
 const AdminConsoleScreen = () => {
-    const user = useSelector((state) => state.user);
-    const { userInfo } = user;
-    const location = useLocation();
+  const user = useSelector((state) => state.user);
+  const { userInfo } = user;
+  const location = useLocation();
 
-    return userInfo && userInfo.isAdmin === 'true' ? (
-        <Box p='20px' minH='100vh'>
-          <Stack direction={{ base: 'column', lg: 'row' }} align={{ lg: 'flex-start' }}>
-            <Stack pr={{ base: 0, md: 14 }} spacing={{ base: 8, md: 10 }} flex='1.5' mb={{ base: 12, md: 'none' }}>
-              <Heading fontSize='2xl' fontWeight='extrabold'>
-                Admin Console
-              </Heading>
-              <Tabs size='md' variant='enclosed'>
-                <TabList>
-                  <Tab>Users</Tab>
-                  <Tab>Products</Tab>
-                  <Tab>Reviews</Tab>
-                  <Tab>Orders</Tab>
-                </TabList>
+return userInfo && userInfo.isAdmin === 'true' ? (
+  <Box p='20px' minH='100vh'>
+   <Stack direction={{ base: 'column', lg: 'row' }} align={{ lg: 'flex-start' }}>
+    <Stack pr={{ base: 0, md: 14 }} spacing={{ base: 8, md: 10 }} flex='1.5' mb={{ base: 12, md: 'none' }}>
+     <Heading fontSize='2xl' fontWeight='extrabold'>
+      Admin Console
+     </Heading>
+      <Tabs size='md' variant='enclosed'>
+       <TabList>
+        <Tab>Users</Tab>
+        {/* <Tab>Products</Tab>
+        <Tab>Reviews</Tab> */}
+        <Tab>Orders</Tab>
+       </TabList>
         <TabPanels>
+
          <TabPanel>
           <UsersTab/>
-         </TabPanel>   
+         </TabPanel> 
+
+         <TabPanel>
+          <OrdersTab/>
+         </TabPanel> 
+           
         </TabPanels>
-     </Tabs>
-    </Stack>   
+       </Tabs>
+      </Stack>   
     </Stack>
    </Box>
   ) : (
